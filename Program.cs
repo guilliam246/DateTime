6d7d2dd58c886d180350401a8a3560f63e0c4eb8
Guilliam Oberholster <guilliamoberholster@gmail.com>
2022-05-30 11:17:11 +1000

    adding more functions

--- Program.cs
+++ Program.cs
@@ -37,6 +37,19 @@ namespace DateTimeEX
 
             Console.WriteLine("Minutes: "+ timeSpan.Minutes);
             Console.WriteLine("Totla Minutes: " + timeSpan.TotalMinutes);
+
+            //Add and subtract
+
+            Console.WriteLine("Add Example: " + timeSpan.Add(TimeSpan.FromMinutes(8)));
+            Console.WriteLine("Add Example: " + timeSpan.Subtract(TimeSpan.FromMinutes(2)));
+
+            //ToString
+
+            Console.WriteLine("To String" + timeSpan.ToString());
+        
+            //Parse
+
+            Console.WriteLine("Parse: "+ TimeSpan.Parse("01:02:03"));    
         }
     }
 }
