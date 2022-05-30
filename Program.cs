2979b3eb3665014876b0e9d3db640292e655faa7
Guilliam Oberholster <guilliamoberholster@gmail.com>
2022-05-30 10:45:56 +1000

    Different ways to format it as a string

--- Program.cs
+++ Program.cs
@@ -10,8 +10,16 @@ namespace DateTimeEX
             var now = DateTime.Now;
             var today = DateTime.Today;
 
-            Console.WriteLine("Hour: " + now.Hour);
-            Console.WriteLine("Minute: " + now.Minute);
+          //  Console.WriteLine("Hour: " + now.Hour);
+          //  Console.WriteLine("Minute: " + now.Minute);
+
+            var tomorrow = now.AddDays(1);
+            var yesterday = today.AddDays(-1);
+
+            Console.WriteLine(now.ToLongDateString());
+            Console.WriteLine(now.ToShortDateString ());
+            Console.WriteLine(now.ToLongTimeString());
+            Console.WriteLine(now.ToShortTimeString());
         }
     }
 }
